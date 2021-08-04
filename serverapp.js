3a069942cd4bbc6da444fbc05c7d340001a6251c
Aima Atigari <36767198+illuminateme@users.noreply.github.com>
2021-08-04 07:29:23 +0100

    Delete serverapp.js

--- serverapp.js
+++
@@ -1,5 +0,0 @@
-var http = require('http');
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('You would discover that I am all you want')
-}).listen(8080);
